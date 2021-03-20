ee7fd6bb5c5c5827038f1391230232ca8978c7b7
Glitch (reservation-assignment-app-bc) <none>
2021-03-20 23:42:28 +0000

    Arrays Updated

--- public/script.js
+++ public/script.js
@@ -3,3 +3,18 @@ $(".deleteBuilding").click(() => confirm('Are you sure you want to delete this b
 $(".deleteCampus").click(() => confirm('Are you sure you want to delete this campus?'));
 $(".deleteRoom").click(() => confirm('Are you sure you want to delete this room?'));
 $(".deleteClass").click(() => confirm('Are you sure you want to delete this class?'));
+
+function initMap() {
+const home = { lat: 52.24297, lng: -6.95122 };
+// The map, centered at Uluru
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 15,
+  center: home,
+});
+
+// The marker, positioned at Uluru
+const marker = new google.maps.Marker({
+  position: home,
+  map: map,
+});
+}
